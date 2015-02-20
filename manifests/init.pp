99c8cc71c45653683277ec83a601b083112b625e
Florian Thiemer <florian.thiemer@fokus.fraunhofer.de>
2015-02-20 16:02:26 +0100

    added tree structure for liferay config files

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,10 @@
 
 class liferay ($jbosshome){
 
+	$liferay_dirs = ["$jbosshome/modules/com", "$jbosshome/modules/com/portal", "$jbosshome/modules/com/portal/main]
+
 	# Create the folder $jbosshome/modules/com/liferay/portal/main
-	file { '/opt/jboss/modules/com/liferay/portal/main':
+	file { $liferay_dirs:
 		ensure => 'directory',
 	}
 	
