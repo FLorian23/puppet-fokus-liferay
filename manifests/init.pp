79fc2487d69d5c90d481641b3cb3f33ab32092cd
Florian Thiemer <florian.thiemer@fokus.fraunhofer.de>
2015-02-20 14:49:33 +0100

    added missing ','

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class liferay ($jbosshome){
 
 	# Create the folder $jbosshome/modules/com/liferay/portal/main
 	file { '$jbosshome/modules/com/liferay/portal/main':
-		ensure => 'directory'
+		ensure => 'directory',
 	}
 	
 }
